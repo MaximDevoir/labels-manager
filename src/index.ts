43ff0f355256c4d1e6d4659bdc4c569032129cab
Maxim Devoir <maximdevoir@gmail.com>
2019-08-26 22:24:27 -0700

    Achieved viewing all labels

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,51 @@
-import { Application } from 'probot'
+import { Application, Context } from 'probot'
 
 function isMasterBranch(ref: string): boolean {
   return ref === 'refs/heads/master'
 }
+
+type Label = {
+  color: string
+  default: boolean
+  id: string
+  name: string
+  node_id: string
+  url: string
+}
+
+async function getLabels(
+  client: Context,
+  owner: string,
+  repo: string
+): Promise<Pick<Label, 'name' | 'id'>[] | null> {
+  const  result = await client.github.graphql(
+    `query Labels($repo: String!, $owner: String!) {
+      repository(name: $repo, owner: $owner) {
+        labels(first: 100) {
+          nodes {
+            id
+            name
+            description
+            color
+          }
+        }
+      }
+    }
+  `,
+    {
+      repo,
+      owner
+    }
+  )
+
+  if (result.errors) {
+    return null
+  }
+
+  const labels = (result as any)
+  return labels
+}
+
 
 export = (app: Application): void => {
   app.on('push', async context => {
@@ -32,30 +75,20 @@ export = (app: Application): void => {
     } = await context.github.git.getTree({
       owner,
       repo,
-      tree_sha: commitHash
+      tree_sha: commitHash,
+      recursive: 1
     })
 
     app.log('tree', tree)
 
-    const { data: x } = await context.github.graphql(`{
-      rateLimit(dryRun: false) {
-        limit
-        remaining
-      }
-      repository(owner: "MaximDevoir", name: "create-nom-app") {
-        labels(first: 100) {
-          edges {
-            node {
-              name
-              description
-              color
-            }
-          }
-        }
-      }
-    }`)
+    app.log('attempting')
+    const labels = await getLabels(context, owner, repo)
 
-    app.log('x', x)
+    if (!labels) {
+      return context.log('Error while retrieving labels.')
+    }
+
+    context.log('labels', labels)
   })
   // For more information on building apps:
   // https://probot.github.io/docs/
