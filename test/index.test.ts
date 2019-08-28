20e139f3d040d3a964067072166d5a835b8860a8
Maxim Devoir <maximdevoir@gmail.com>
2019-08-28 04:54:02 -0700

    Initial empty test

---
+++ test/index.test.ts
@@ -0,0 +1,10 @@
+import nock from 'nock'
+import { Probot } from 'probot'
+
+import pushPayload from './fixtures/labels-manager-testing.push.json'
+
+describe('Labels Manager', () => {
+  it('does a thing', async () => {
+
+  })
+})
