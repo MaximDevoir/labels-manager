a5132340ab729853bce79b5b02444c7e62b12329
Maxim Devoir <maximdevoir@gmail.com>
2019-08-28 02:27:19 -0700

    Converted linter configuration to JS module
    - Added "import/no-unresolved" rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
-{
+module.exports = {
   "extends": [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended"
   ],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
@@ -17,5 +17,8 @@
     "jest": true
   },
   "rules": {
+    // Imports are failing for the linter, set rule off - warn doesn't work for
+    // this rule. We may need to add eslint-import-resolver-typescript
+    "import/no-unresolved": "off"
   }
 }
