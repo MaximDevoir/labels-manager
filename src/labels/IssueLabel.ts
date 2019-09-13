ee1ef3a2ef7abdea67d3e9625c8f615624183724
Maxim Devoir <maximdevoir@gmail.com>
2019-09-12 19:28:41 -0700

    Abstract the fetching of labels to `IssueLabels` and `IssueLabel`

--- src/labels/IssueLabel.ts
+++ src/labels/IssueLabel.ts
@@ -1,5 +1,19 @@
+import IssueLabels from "./IssueLabels";
+
+export interface IIssueLabel {
+  id: number;
+  node_id: string;
+  name: string,
+  color: string,
+  description: string
+  default: boolean;
+  url: string;
+}
+
 class IssueLabel {
-  constructor () {}
+  constructor (public label: IIssueLabel) {
+
+  }
 }
 
 export default IssueLabel
--- src/labels/IssueLabels.ts
+++ src/labels/IssueLabels.ts
@@ -1,19 +1,49 @@
 import Labels from "../Labels";
 import { Label } from "../query/RepoLabels";
 import { Context } from "probot";
+import IssueLabel, { IIssueLabel } from "./IssueLabel";
+import GetIssueLabelTotalCount from "../query/IssueLabelsCount";
+import LabelsError from "../reporter/LabelsError";
 
 interface IssueLabelsCollection {
-  [key: string]: Label
+  [key: string]: IssueLabel
 }
+
+type UnPromisify<T> = T extends Promise<infer U> ? U : T;
 
 class IssueLabels {
   public labels: IssueLabelsCollection = {}
+  public totalCount: ReturnType<GetIssueLabelTotalCount['fire']>
   constructor (private context: Context) {
-
+    this.totalCount = new GetIssueLabelTotalCount(this.context).fire()
   }
 
-  getIssueLabels() {
-    // this.context.github.issues.label
+  private async checkLabelCount() {
+    const issuesLabelTotalCount = (await this.totalCount).repository.labels.totalCount
+    const issuesLabelLimit = 256
+
+    if (issuesLabelTotalCount > issuesLabelLimit) {
+      throw new LabelsError(this.context, {
+        title: 'Repository exceeds label limit.',
+        summary: [`This repository contains ${issuesLabelTotalCount} labels - more than the ${issuesLabelLimit} label limit.`]
+      })
+    }
+  }
+
+  async getIssueLabels() {
+    await this.checkLabelCount()
+    const labels = await this.context.github.paginate(this.context.github.issues.listLabelsForRepo.endpoint.merge({
+      ...this.context.repo(),
+      per_page: 100
+    })) as UnPromisify<ReturnType<Context['github']['issues']['listLabelsForRepo']>>['data']
+
+    labels.forEach(label => {
+      this.addLabel(label)
+    })
+  }
+
+  private addLabel(label: IIssueLabel) {
+    this.labels[label.name] = new IssueLabel(label)
   }
 }
 
