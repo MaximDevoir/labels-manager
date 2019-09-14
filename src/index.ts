6f3ccf812a283d742750c27ecbf86d4e35f42cd8
Maxim Devoir <maximdevoir@gmail.com>
2019-09-14 13:14:48 -0700

    Log unknown errors

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,39 @@ import RepoLabels from './query/RepoLabels';
 
 import { WebhookPayloadPush } from '@octokit/webhooks'
 import Job from './Job';
+import LabelsError from './reporter/LabelsError';
 
 export = (app: Application): void => {
   app.on('push', async context => {
     const job = new Job(context)
 
-    job.sync()
+    try {
+      await job.sync()
+    } catch (e) {
+      const errorName = e.name
+      const recognizedErrors = ['JSONError', 'LabelsError']
+      if (recognizedErrors.includes(errorName)) {
+        throw e
+      }
+
+      throw new LabelsError(context, {
+        title: 'Unhandled Error Encountered',
+        summary: [
+          'The application encountered an unhandled error.'
+        ],
+        text: [
+          '### Error Information',
+          `**Name**: \`${e.name}\``,
+          `**Type (typeof)**: \`${typeof e}\``,
+        ]
+      },
+      'error', e,
+      '#### Error Stack',
+      ...(typeof e.stack === 'string' ? ['```', e.stack, '```'] : ['No stack available', `Stack is \`${typeof e.stack}\``]),
+
+      )
+    }
+
 
     // const labels = new Labels(context, owner, repo)
     // await labels.getAllLabels()
@@ -19,6 +46,7 @@ export = (app: Application): void => {
     // const x = await repoLabels.getRepoLabels()
     // console.log(x)
   })
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
