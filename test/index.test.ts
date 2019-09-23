b625883f5a7d07b8fd654549e2362b4e439635b5
Maxim Devoir <maximdevoir@gmail.com>
2019-09-23 14:30:52 -0700

    WIP basic test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,37 @@
 import nock from 'nock'
 import { Probot } from 'probot'
 
-import pushPayload from './fixtures/labels-manager-testing.push.json'
+import labelsManager from './../src/index'
+import pushPayload from './fixtures/labels-manager-testing.push'
+import { rateLimitResourcesResponse } from './fixtures/rate_limit'
+
+nock.disableNetConnect()
 
 describe('Labels Manager', () => {
-  it('does a thing', async () => {
+  let probot: Probot
 
+  beforeEach(() => {
+    probot = new Probot({ id: 123, cert: 'test' })
+    const app = probot.load(labelsManager)
+
+    app.app = {
+      getSignedJsonWebToken() {
+        return 'test-token'
+      },
+      getInstallationAccessToken() {
+        return Promise.resolve('test-token')
+      }
+    }
   })
+
+  // it('does a thing', async () => {
+  //   const pushNock = nock('https://api.github.com')
+  //     .get('/rate_limit')
+  //     .reply(200, rateLimitResourcesResponse)
+  //   await probot.receive({
+  //     name: "push",
+  //     id: "test-event-id",
+  //     payload: pushPayload.payload
+  //   })
+  // })
 })
