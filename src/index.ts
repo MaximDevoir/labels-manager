d94e497c606bd270277d816777dc40775c949ef3
Maxim Devoir <maximdevoir@gmail.com>
2019-09-23 21:20:29 -0700

    Throw unrecognized errors

--- src/index.ts
+++ src/index.ts
@@ -10,32 +10,41 @@ export = (app: Application): void => {
     try {
       await job.sync()
     } catch (e) {
+      // Only throw on unrecognized errors
       const errorName = e.name
       const recognizedErrors = ['JSONError', 'LabelsError']
       if (recognizedErrors.includes(errorName)) {
-        throw e
+        if (e instanceof LabelsError) {
+          e.throwAfterReport = false
+          await e.checkCreation
+        } else {
+          throw e
+        }
+      } else {
+        new LabelsError(context, {
+          title: 'Unhandled Error Encountered',
+          summary: [
+            'The application encountered an unhandled error.'
+          ],
+          text: [
+            '### Error Information',
+            `**Name**: \`${e.name}\``,
+            `**Type (typeof)**: \`${typeof e}\``,
+          ]
+        },
+        'error', e,
+        '#### Error Stack',
+        ...(typeof e.stack === 'string' ? ['```', e.stack, '```'] : ['No stack available', `Stack is \`${typeof e.stack}\``]),
+
+        )
       }
-
-      throw new LabelsError(context, {
-        title: 'Unhandled Error Encountered',
-        summary: [
-          'The application encountered an unhandled error.'
-        ],
-        text: [
-          '### Error Information',
-          `**Name**: \`${e.name}\``,
-          `**Type (typeof)**: \`${typeof e}\``,
-        ]
-      },
-      'error', e,
-      '#### Error Stack',
-      ...(typeof e.stack === 'string' ? ['```', e.stack, '```'] : ['No stack available', `Stack is \`${typeof e.stack}\``]),
-
-      )
     } finally {
+      context.log('Finally...')
       // Attempts to log metrics if not already logged. This will miss any resource costs incurred during the `catch` phase.
       await job.endMetrics()
     }
+
+    context.log('fin')
   })
 
   // For more information on building apps:
